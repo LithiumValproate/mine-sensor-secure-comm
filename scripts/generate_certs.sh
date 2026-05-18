c8cdf2d04bbcf38d7e2ae79e5febc6cacc0d61ff
LithiumValproate <kisara@folate.li>
2026-05-18 15:45:58 +0800

    Updated `generate_certs.sh` to use relative paths and switched to zsh

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -1,23 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 set -euo pipefail
 
-mkdir -p certs
+mkdir -p ../certs
 
-openssl genrsa -out certs/ca.key 4096
-openssl req -x509 -new -nodes -key certs/ca.key -sha256 -days 3650 \
+openssl genrsa -out ../certs/ca.key 4096
+openssl req -x509 -new -nodes -key ../certs/ca.key -sha256 -days 3650 \
   -subj "/CN=mine-local-ca" \
-  -out certs/ca.crt
+  -out ../certs/ca.crt
 
 create_cert() {
   local name="$1"
   local cn="$2"
   local san="$3"
 
-  openssl genrsa -out "certs/${name}.key" 2048
-  openssl req -new -key "certs/${name}.key" -subj "/CN=${cn}" -out "certs/${name}.csr"
-  printf "subjectAltName=%s\nextendedKeyUsage=serverAuth,clientAuth\n" "$san" > "certs/${name}.ext"
-  openssl x509 -req -in "certs/${name}.csr" -CA certs/ca.crt -CAkey certs/ca.key \
-    -CAcreateserial -out "certs/${name}.crt" -days 825 -sha256 -extfile "certs/${name}.ext"
+  openssl genrsa -out "../certs/${name}.key" 2048
+  openssl req -new -key "../certs/${name}.key" -subj "/CN=${cn}" -out "../certs/${name}.csr"
+  printf "subjectAltName=%s\nextendedKeyUsage=serverAuth,clientAuth\n" "$san" > "../certs/${name}.ext"
+  openssl x509 -req -in "../certs/${name}.csr" -CA ../certs/ca.crt -CAkey ../certs/ca.key \
+    -CAcreateserial -out "../certs/${name}.crt" -days 825 -sha256 -extfile "../certs/${name}.ext"
 }
 
 create_cert broker localhost "DNS:localhost,IP:127.0.0.1"
@@ -26,5 +26,5 @@ create_cert temperature_sensor_01 temperature_sensor_01 "DNS:temperature_sensor_
 create_cert gas_sensor_01 gas_sensor_01 "DNS:gas_sensor_01"
 create_cert gas_sensor_02 gas_sensor_02 "DNS:gas_sensor_02"
 
-rm -f certs/*.csr certs/*.ext certs/ca.srl
-chmod 600 certs/*.key
+rm -f ../certs/*.csr ../certs/*.ext ../certs/ca.srl
+chmod 600 ../certs/*.key
